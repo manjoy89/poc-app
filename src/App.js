a867b825118d0f2983d95bed8c6471d9d2ed5777
Manoj Kumar Mohan <manjoy89@gmail.com>
2020-04-10 01:42:05 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return(
+        <Footer>Gooter</Footer>
+    )
+}
+
+export default Footer;
